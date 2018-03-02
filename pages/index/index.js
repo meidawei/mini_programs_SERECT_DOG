9ee92cc73a20594af48a17b339a31eebb6b3607f
DaShuaiGe <2789002425@qq.com>
2018-03-02 16:07:06 +0800

    替换base的加密js解决中文加密后乱码问题

--- pages/index/index.js
+++ pages/index/index.js
@@ -16,23 +16,26 @@ var pageObject = {
     text_new: "",
     text_after: ""
   },
+  //获取第一个textarea的值，传递给text_new
+  getFirstTextarea: function (e) {
+    this.setData({
+      text_new: e.detail.value
+    })
+  },
   //加密按钮
   getSerect: function (e) {
     var self = this;
-    console.log(e.detail.value);
-    return;
     self.setData({
       loading: true
     })
-    var obj_base64 = new BASE64.Base64();
     //base64加密
-    var text_new = e.detail.value;
+    var text_new = self.data.text_new;
     var text_after = "";
 
     switch (self.data.index) {
       //base 64加密
       case 0:
-        var str_base64_encode = obj_base64.encode(text_new);
+        var str_base64_encode = BASE64.CusBASE64.encoder(text_new);
         text_after = str_base64_encode;
     }
     //base64解密
@@ -44,7 +47,7 @@ var pageObject = {
         loading: false,
         text_after: text_after
       });
-    }, 1500)
+    }, 500)
   },
   //单选事件选择加密方式
   bindPickerChange: function (e) {
